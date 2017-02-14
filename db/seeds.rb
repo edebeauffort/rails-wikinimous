04525832eb85a8ca8a7cf01bd02724603ab32518
edouard de beauffort <e.debeauffort@gmail.com>
2017-02-14 15:04:34 +0000

    ex 2 finished, will try optional elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  Article.create! \
+    title: Faker::Company.name,
+    content: Faker::Company.catch_phrase
+end
